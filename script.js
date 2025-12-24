46e808168d3752e9fea9be9c89ad0605408b0e58
German Martinez <pocolar95@gmail.com>
2025-12-24 14:34:44 -0300

    Agregando js minimo primera instancia

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnEs = document.getElementById("btn-es");
+const btnEn = document.getElementById("btn-en");
+const body = document.body;
+
+btnEs.addEventListener("click", () => {
+    body.classList.remove("lang-en-active");
+    body.classList.add("lang-es-active");
+});
+
+btnEn.addEventListener("click", () => {
+    body.classList.remove("lang-es-active");
+    body.classList.add("lang-en-active");
+});
