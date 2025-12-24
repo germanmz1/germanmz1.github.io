a3ebdc9b14ae028c14daa7d361c0e396604ca700
German Martinez <pocolar95@gmail.com>
2025-12-24 14:59:49 -0300

    Fixeo numero 2

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const btnEn = document.getElementById("btn-en");
 const body = document.body;
 
 btnEs.addEventListener("click", () => {
-  body.classList.remove("lang-en-active");
-  body.classList.add("lang-es-active");
+    body.classList.remove("lang-en-active");
+    body.classList.add("lang-es-active");
 });
 
 btnEn.addEventListener("click", () => {
-  body.classList.remove("lang-es-active");
-  body.classList.add("lang-en-active");
+    body.classList.remove("lang-es-active");
+    body.classList.add("lang-en-active");
 });
